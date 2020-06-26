5c8d516f2b67db9425fc8505c48d63b3713c8c78
Kevin Huynh <anmevee@yahoo.com>
2020-06-26 13:07:33 -0700

    fixed scraper showing only last page of servants and modified to write to json file

--- servant_scraper.py
+++ servant_scraper.py
@@ -3,30 +3,32 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys  
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
+import json
 
 # Array containing all the urls that contains the urls to the servant profiles
 websites = ['https://fategrandorder.fandom.com/wiki/Sub:Servant_List_by_ID/1-100', 'https://fategrandorder.fandom.com/wiki/Sub:Servant_List_by_ID/101-200', 'https://fategrandorder.fandom.com/wiki/Sub:Servant_List_by_ID/201-300']
 servant_url = []
+all_servant_info = {}
 # For each url, we open the url using selenium (because some html code is loaded dynamically)
 # Then get the page source
 for url in websites:
     browser = webdriver.Chrome(executable_path=r"/mnt/c/Users/Kevin Huynh/Documents/chromedriver.exe")
-    browser.get(url)  
+    browser.get(url)
     html_source = browser.page_source  
     browser.quit()
 
-# We extract all the <a> tags within a specific container
-soup_html = soup(html_source,'lxml')  
-container = soup_html.find('table', {'class':'wikitable sortable jquery-tablesorter'})
-servants = container.findAll('a');
+    # We extract all the <a> tags within a specific container
+    soup_html = soup(html_source,'lxml')  
+    container = soup_html.find('table', {'class':'wikitable sortable jquery-tablesorter'})
+    servants = container.findAll('a');
 
-# For each <a> tag, check if it's an img. If so, continue.
-# Else append the <href> to an array.
-for servant in servants:
-    if 'class' in servant.attrs:
-        continue
-    else:
-        servant_url.append('https://fategrandorder.fandom.com' + servant.attrs['href'])
+    # For each <a> tag, check if it's an img. If so, continue.
+    # Else append the <href> to an array.
+    for servant in servants:
+        if 'class' in servant.attrs:
+            continue
+        else:
+            servant_url.append('https://fategrandorder.fandom.com' + servant.attrs['href'])
 
 # For each servant, extract necessary information for database
 for url in servant_url:
@@ -42,13 +44,18 @@ for url in servant_url:
     servant_name = profile_container.find('p', {'class': 'ServantInfoName'}).b.string
     servant_class = profile_container.find('div', {'class': 'ServantInfoClass'}).a.attrs['title']
     servant_rarity = profile_container.find('div', {'class': 'ServantInfoStars'}).string
-
     profile_container_wrapper = profile_container.find('div', {'class': 'ServantInfoStatsWrapper'})
+    print(servant_name)
+    # The rows are not the actual rows of the table, but the rows that we're interested in
     first_row = profile_container_wrapper.table.tr.find_next_sibling('tr')
+
+    # Many servants have alternative names, so we must check to skip over
     if first_row.td.span != None and first_row.td.span.attrs['title'] == 'Also Known As':
         first_row = first_row.find_next_sibling('tr')
+
     servant_id = first_row.td.b.next_sibling.strip()
     servant_cost = first_row.td.find_next_sibling('td').b.next_sibling.strip()
+
     second_row = first_row.find_next_sibling('tr')
     servant_atk = second_row.td.span.next_sibling.strip().split('/')
     servant_hp = second_row.td.find_next_sibling('td').span.next_sibling.strip().split('/')
@@ -75,12 +82,14 @@ for url in servant_url:
     eigth_row = seventh_row.find_next_sibling('tr')
     gender = eigth_row.td.b.next_sibling.strip()
 
+    # Traits and card types are in their own separate table
     first_row = profile_container_wrapper.table.find_next_sibling('table').tr
     traits = first_row.td.b.next_sibling.strip().split(', ')
 
     second_row = first_row.find_next_sibling('tr')
     cards = second_row.th.noscript.img.attrs['alt']
 
+    # Each servants have three skills, but some may be upgraded
     skills = {}
     div_skill_titles = [{'title': 'First Skill'}, {'title': 'Second Skill'}, {'title': 'Third Skill'}]
 
@@ -92,8 +101,10 @@ for url in servant_url:
             # For each skill (there can be multiple via upgrade),
             # find the skill information (name, rank, effect, skillups, cooldown)
             for skill in skill_container:
+
                 if skill.tr.th != None:
                     continue
+
                 skill_name = skill.td.find_next_sibling('td').a.string
                 skill_rank = skill.td.find_next_sibling('td').a.next_sibling.strip()
                 skill_effects = skill.tr.find_next_sibling('tr').findAll('td')
@@ -125,12 +136,15 @@ for url in servant_url:
         except:
             continue
 
-
-
-    header_iterator = soup_html.findAll('h2')
     passive_skills = {}
     np_info = {}
     bond_ce = None
+
+    # Find all the headers (i.e. Skills, Noble Phantasm, Biography, etc.)
+    header_iterator = soup_html.findAll('h2')
+
+    # Loops through all the headers to search for specific ones
+    # Once we've reached the header we want, we extract info from the table
     for header in header_iterator:
         if header.span != None and header.span.string != None:
             label = header.span.string.strip()
@@ -138,9 +152,13 @@ for url in servant_url:
             if label == 'Passive Skills':
                 passive_skill_container = header.find_next_sibling()
                 if passive_skill_container.name == 'div':
-                    passive_skill_list = passive_skill_container.div.table.findAll('tr')
+                    try:
+                        passive_skill_list = passive_skill_container.div.table.findAll('tr')
+                    except:
+                        continue
                 else:
                     passive_skill_list = passive_skill_container.findAll('tr')
+
                 # If ps contains a string inside <td>, then it contains the name and rank of passive skill
                 # The row directly after consists of all the effects of the passive skill
                 for ps in passive_skill_list:
@@ -160,11 +178,13 @@ for url in servant_url:
                     except:
                         continue
 
+
             elif label == 'Noble Phantasm':
                 np_container = header.find_next_sibling('div').div
 
                 while np_container != None and np_container.attrs['title'].strip() != 'Video':
                     try:
+                        
                         np_table = np_container.table
                         if np_table.caption == None:
                             np_table = np_table.find_next_sibling('table')
@@ -189,6 +209,7 @@ for url in servant_url:
                         np_oc = [oc.string.strip().replace('\n', '') for oc in np_fifth_row.findAll('td')]
 
                         np_container = np_container.find_next_sibling('div')
+
                     except:
                         np_container = np_container.find_next_sibling('div')
                         continue
@@ -203,28 +224,43 @@ for url in servant_url:
                     while bond_ce_iter.th.string == None or bond_ce_iter.th.string.strip() != 'Bond 10 Reward':
                         bond_ce_iter = bond_ce_iter.find_next_sibling('tr')
 
-                    bond_ce = bond_ce_iter.td.find_next_sibling('td').b.a.string.strip()
+                    all_text = [text.strip() for text in bond_ce_iter.td.find_next_sibling('td').findAll(text=True) if len(text) > 1]
+                    bond_ce = all_text[0]
 
-    print(servant_name)
-    print(servant_class)
-    print(servant_rarity)
-    print(servant_id)
-    print(servant_cost)
-    print(servant_atk[0], servant_atk[1])
-    print(servant_hp[0], servant_hp[1])
-    print(voice_actor)
-    print(illustrator)
-    print(attribute)
-    print(star_absorbtion)
-    print(star_generation)
-    print(np_charge_atk)
-    print(np_charge_def)
-    print(death_rate)
-    print(alignment)
-    print(gender)
-    print(traits)
-    print(cards)
-    print(skills)
-    print(passive_skills)
-    print(np_info)
-    print(bond_ce)
+    if len(servant_atk) == 1:
+        servant_atk.append(servant_atk[0])
+        servant_atk[0] = None
+    if len(servant_hp) == 1:
+        servant_hp.append(servant_hp[0])
+        servant_hp[0] = None
+
+    servant_data = {
+        'ID': servant_id,
+        'Gender': gender,
+        'Class': servant_class,
+        'Rarity': len(servant_rarity.split()),
+        'Cost': servant_cost,
+        'Min Atk': servant_atk[0],
+        'Max Atk': servant_atk[1],
+        'Min HP': servant_hp[0],
+        'Min HP': servant_hp[1],
+        'Star Absorbtion': star_absorbtion,
+        'Star Generation': star_generation,
+        'NP Charge Atk': np_charge_atk,
+        'NP Charge Def': np_charge_def,
+        'Death Rate': death_rate,
+        'Attribute': attribute,
+        'Alignment': alignment,
+        'Traits': traits,
+        'Cards': cards,
+        'Skills': skills,
+        'Passives': passive_skills,
+        'Noble Phantasm': np_info,
+        'Voice Actor': voice_actor,
+        'Illustrator': illustrator,
+        'Bond CE': bond_ce
+    }
+    all_servant_info[servant_name] = servant_data
+
+with open('servant_details.json', 'w') as file:
+    json.dump(all_servant_info, file, indent=4)
