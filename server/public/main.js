6791c4e1a784356a0ebc952acaaef98780cbc942
Kevin Huynh <anmevee@yahoo.com>
2020-07-29 12:43:01 -0700

    added additional inputs to dmgcalc

--- server/public/main.js
+++ server/public/main.js
@@ -58,3 +58,63 @@ function display(x) {
         document.getElementById('roll-form').style.display='block';
     }
 }
+
+function enable_servant_select() {
+    const class_selected = document.getElementById('class-select').value;
+    document.getElementById('ATK').value = '0';
+    document.getElementById('NPMod').value = '0';
+    if(class_selected != 'None') {
+        let servant_class = [];
+        let servant_select_id = document.getElementById('servant-select');
+        let servants = JSON.parse(document.getElementById('servants').value);
+
+        $('#servant-select').empty().append('<option value="None" selected>Choose a servant...</option>');
+        servant_select_id.disabled = false;
+
+        if(class_selected == 'All') {
+            servant_class = ['Saber', 'Lancer', 'Archer', 'Rider', 'Caster', 'Assassin', 'Berserker', 
+                             'Alter Ego', 'Foreigner', 'Ruler', 'Avenger', 'Moon Cancer'];
+        } else if(class_selected == 'Extra') {
+            servant_class = ['Alter Ego', 'Foreigner', 'Ruler', 'Avenger', 'Moon Cancer'];
+        } else {
+            servant_class.push(class_selected);
+        }
+        console.log(servant_class)
+        for(let servant in servants) {
+            if(servant_class.includes(servants[servant]['class_name'])) {
+                let option = document.createElement('option')
+                option.appendChild(document.createTextNode(servants[servant]['name']));
+                option.value = servants[servant]['name'];
+                servant_select_id.append(option);
+            }
+        }
+    } else {
+        document.getElementById('servant-select').disabled = true;
+    }
+
+}
+
+function fill_out_inputs() {
+    let servant = document.getElementById('servant-select');
+    let servants = JSON.parse(document.getElementById('servants').value);
+    servants.forEach(ele => {
+        if(ele['name'] == servant.value) {
+            let atk_ele = document.getElementById('ATK');
+            let np_ele = document.getElementById('NPMod');
+            atk_ele.value = ele['max_atk'].replace(',', '');
+            np_ele.value = ele['np_modifier'].replace(/,|%/g, '');
+
+            return;
+        }
+    })
+}
+
+function check_validity(event, cap) {
+    const old_val = document.getElementById('ATKMod').value;
+    const input = event.key;
+    const reg = new RegExp(/^[0-9]*(\.[0-9]{0,2})?$/, 'g');
+    const new_val = old_val + input;
+    
+    if(reg.test(new_val) && parseFloat(new_val) <= cap) { return true; }
+    else { return false; }
+}
--- server/routes/Calculators/dmgcalc.js
+++ server/routes/Calculators/dmgcalc.js
@@ -4,12 +4,22 @@ const database_manager = require('../../database/database-manager.js');
 
 router.get('/DMGCalculator', async (req, res) => {
   const servants = await database_manager.queryDatabase(`
-  SELECT servants.name, classes.class_name 
-  FROM servants 
-  INNER JOIN classes ON servants.class_id = classes.class_id
-  ORDER BY servants.servant_id ASC;`, []);
-
+    SELECT servants.name, classes.class_name, servants.max_atk, npl.np_modifier 
+    FROM servants 
+    INNER JOIN classes ON servants.class_id = classes.class_id
+    INNER JOIN \`noble phantasms\` AS np ON servants.servant_id = np.servant_id 
+    INNER JOIN \`noble phantasm levels\` AS npl ON np.np_id = npl.np_id
+    WHERE effect LIKE '%Deals damage%' AND npl.np_modifier LIKE '%\%%' AND npl.level = 5 
+    ORDER BY servants.servant_id ASC;`, []);
   res.render('dmgcalc', {servants: JSON.stringify(servants)});
 });
 
 module.exports = router;
+
+/*
+SELECT np.servant_id, npl.np_modifier 
+FROM `noble phantasms` np 
+INNER JOIN `noble phantasm levels` npl 
+ON np.np_id = npl.np_id 
+WHERE effect LIKE '%Deals damage%' AND npl.np_modifier LIKE '%\%%' AND npl.level = 5;
+*/
