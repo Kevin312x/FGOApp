bcf98c6a5d9d9c0151cb86b163ca78537edb047d
Kevin Huynh <anmevee@yahoo.com>
2020-09-22 15:16:41 -0700

    added ellipses to paginator, but doesn't goto page

--- server/routes/middlewares.js
+++ server/routes/middlewares.js
@@ -12,18 +12,19 @@ const paginated_results = (list) => {
             if(end_index < ce.length) {
                 ce_list_result.next = {
                 page: page + 1,
-                limit: limit
                 }
             }
             
             if(start_index > 0) {
                 ce_list_result.prev = {
                 page: page - 1,
-                limit: limit
                 }
             }
             
+            ce_list_result.current_page = page;
+            ce_list_result.limit = limit;
             ce_list_result.result = ce.slice(start_index, end_index);
+            ce_list_result.ce_length = ce.length;
             res.ce_list_result = ce_list_result;
             next();
         });
