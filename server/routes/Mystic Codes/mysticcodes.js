43706fd4756e4b4f6be76a83b10f700265d2c52a
Kevin Huynh <anmevee@yahoo.com>
2020-09-29 11:26:57 -0700

    added: order to mc_list rendered by node

--- server/routes/Mystic Codes/mysticcodes.js
+++ server/routes/Mystic Codes/mysticcodes.js
@@ -6,7 +6,8 @@ const mc_list = database_manager.queryDatabase(`
   SELECT mc.mystic_code_id, mc.mystic_code, images.path 
   FROM \`mystic codes\` AS mc 
   INNER JOIN \`mystic code images\` AS mci ON mc.mystic_code_id = mci.mystic_code_id 
-  INNER JOIN images ON mci.mc_image_id = images.image_id;`, {}
+  INNER JOIN images ON mci.mc_image_id = images.image_id 
+  ORDER BY mc.mystic_code_id ASC;`, {}
 );
 
 router.get('/mystic_code', (req, res) => {
