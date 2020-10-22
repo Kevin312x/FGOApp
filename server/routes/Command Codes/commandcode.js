a8bf100817bcd26d756c718afa9fab91ce7d97c8
Kevin Huynh <anmevee@yahoo.com>
2020-10-22 15:13:04 -0700

    added: route for cc profiles added: cc profile view

--- server/routes/Command Codes/commandcode.js
+++ server/routes/Command Codes/commandcode.js
@@ -19,7 +19,38 @@ router.get('/command_code', async (req, res) => {
     case 'html':
       res.render('command_code', {'cc_list': cc_list_result});
       return;
+    default:
+      break;
   }
+
+  res.status(400).send('Bad Request');
+});
+
+router.get('/command_code/:name', async (req, res) => {
+  const cc_name = req.params.name.replace('_', ' ');
+  
+  const cc_data = await database_manager.queryDatabase(`
+    SELECT cc.code_id, cc.name, cc.rarity, cc.effect, cc.illustrator, cc.description, images.path 
+    FROM \`command codes\` AS  cc 
+    INNER JOIN \`command code images\` AS cci ON cci.code_id = cc.code_id 
+    INNER JOIN images ON images.image_id = cci.image_id 
+    WHERE cc.name = :name;`, 
+  {
+    name: cc_name
+  });
+
+  switch(req.accepts(['json', 'html'])) {
+    case 'json':
+      res.send({'cc_data': cc_data});
+      return;
+    case 'html':
+      res.render('command_code_profile', {'cc_data': cc_data});
+      return;
+    default:
+      break;
+  }
+
+  res.status(400).send('Bad Request');
 });
 
 module.exports = router;
