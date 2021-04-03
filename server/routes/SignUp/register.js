a4ba4de147bf15b727c10e5b764f51be2525e2ce
Kevin Huynh <anmevee@yahoo.com>
2021-04-03 15:25:42 -0700

    added: hash to password

--- server/routes/SignUp/register.js
+++ server/routes/SignUp/register.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const database_manager = require('../../database/database-manager.js');
-const router = express.Router()
+const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.get('/register', (req, res) => {
   res.render('register');
 });
 
 router.post('/register', async (req, res) => {
-  console.log(req.body)
+  const username = req.body.username;
+  const email = req.body.email;
+
+  try { var hash_pass = await bcrypt.hash(req.body.password, 10); }
+  catch { return res.status(404).send({'error': {'401': 'Unauthorized', 'description': 'Sorry, wrong username or password.'}}); }
+
+  console.log(hash_pass)
 });
 
 module.exports = router;
