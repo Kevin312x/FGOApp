04343de3eeb3ddff86498a8eeae9c5c0c0a29cc0
Kevin Huynh <anmevee@yahoo.com>
2021-04-20 14:45:46 -0700

    modified: removed jquery to load navbar and use ejs partials

--- server/app.js
+++ server/app.js
@@ -37,6 +37,7 @@ const register_router = require('./routes/Users/SignUp/register.js');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(public_path));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine','ejs');
 app.use(passport.initialize());
 app.use(passport.session());
